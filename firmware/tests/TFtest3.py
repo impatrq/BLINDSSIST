86a643f85e869cbf588866e895f581a0af94d840
TiagoQuattrocchi <101597256+TiagoQuattrocchi@users.noreply.github.com>
2025-08-27 14:57:45 -0300

    Update TFtest3.py

--- firmware/tests/TFtest3.py
+++ firmware/tests/TFtest3.py
@@ -12,12 +12,14 @@ def getTFminiData():
         count = ser.in_waiting
         print ("4")
         if count > 8:
+            print ("5")
             recv = ser.read(9)  
             ser.reset_input_buffer()  
             # type(recv), 'str' in python2(recv[0] = 'Y'), 'bytes' in python3(recv[0] = 89)
             # type(recv[0]), 'str' in python2, 'int' in python3 
             if recv[0] == 0x59 and recv[1] == 0x59:
-                #python3
+                
+                print("6")#python3
                 distance = recv[2] + recv[3] * 256
                 strength = recv[4] + recv[5] * 256
                 ser.reset_input_buffer()
